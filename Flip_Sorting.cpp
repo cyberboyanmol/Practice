20476b79409ef349c697ee825d3d3d21e908d0fe
Robin-Anmol <anmolgangwar64@gmail.com>
2022-04-01 12:03:43 +0530

    1ap

---
+++ Flip_Sorting.cpp
@@ -0,0 +1,85 @@
+/*
+********************************************************
+* AUTHOR   : ANMOL GANGWAR                             *
+* LANGUAGE : c++14,c++17                               *
+* IDE      : VS CODE ,codechef(ide)                    *
+********************************************************
+         ================================
+                 KEEP GOING
+              EVERYTHING YOU NEED
+                WILL COME TO YOU
+              AT THE PERFECT TIME
+         ================================
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define int long long
+#define ull unsigned long long
+#define endl "\n"
+#define pb emplace_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define vs vector<string>
+#define mii map<int, int>
+#define pqb priority_queue<int>
+#define lb(x, n) lower_bound(all(x), n)
+#define ub(x, n) upper_bound(all(x), n)
+#define all(x) x.begin(), x.end()
+#define maxi(x) x[max_element(all(x)) - x.begin()]
+#define mini(x) x[min_element(all(x)) - x.begin()]
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define tc(t) \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+void change(string &str, int x)
+{
+    for (int i = x; i < str.length(); i++)
+    {
+        if (str[i] == '0')
+            str[i] = '1';
+        else
+        {
+            str[i] = '0';
+        }
+    }
+}
+int32_t
+main()
+{
+    fast;
+    tc(t)
+    {
+        int N;
+        cin >> N;
+        string str;
+        cin >> str;
+        vector<pair<int, int>> vec_pair;
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str[i] == '1')
+            {
+                vec_pair.push_back({i + 1, N - i});
+                change(str, i);
+            }
+        }
+        cout << vec_pair.size() << endl;
+        for (auto it : vec_pair)
+        {
+            cout << it.first << " " << it.second << endl;
+        }
+    }
+    return 0;
+}
